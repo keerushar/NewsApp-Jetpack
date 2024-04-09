945166bd683c19ba2b06dc9744ddcb1a996baeae
Kiran Sharma <kiran.sharma@gurzu.com>
2024-04-09 16:02:25 +0545

    feat: setup pager view

--- app/src/main/java/com/rushar/newsapp/MainActivity.kt
+++ app/src/main/java/com/rushar/newsapp/MainActivity.kt
@@ -3,14 +3,12 @@ package com.rushar.newsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.rushar.newsapp.presentation.onboarding.OnBoardingScreen
 import com.rushar.newsapp.ui.theme.NewsAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +17,9 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             NewsAppTheme {
-
+                Box(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
+                    OnBoardingScreen()
+                }
             }
         }
     }
