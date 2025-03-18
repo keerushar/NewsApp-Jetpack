ccf4dd9b1bf075cb6eb4edf5c440d194e8f04166
kiransharma <kiran.sharma@gurzu.com>
2025-03-18 14:44:04 +0545

    feat: setup system bars colors

--- app/src/main/java/com/rushar/newsapp/MainActivity.kt
+++ app/src/main/java/com/rushar/newsapp/MainActivity.kt
@@ -5,11 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource.Companion.SideEffect
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.rushar.newsapp.presentation.navgraph.NavGraph
 import com.rushar.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,10 +35,18 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
+
+            val isSystemInDarkMode = isSystemInDarkTheme()
+            val systemUiColor = rememberSystemUiController()
+            SideEffect {
+                systemUiColor.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = !isSystemInDarkMode
+                )
+            }
             NewsAppTheme {
-                Box(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
-                    val startDestination = viewModel.startDestination.value
-                    NavGraph(startDestination = startDestination)
+                Box(modifier = Modifier.background(MaterialTheme.colors.background).fillMaxSize()) {
+                    NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
         }
