a416e79d345a675f6196f13108d09480d9231bc9
kiransharma <kiran.sharma@gurzu.com>
2025-03-18 14:02:09 +0545

    feat: setup nav graph

--- app/src/main/java/com/rushar/newsapp/MainActivity.kt
+++ app/src/main/java/com/rushar/newsapp/MainActivity.kt
@@ -1,38 +1,38 @@
 package com.rushar.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.rushar.newsapp.domain.usecases.AppEntryUseCases
-import com.rushar.newsapp.presentation.onboarding.OnBoardingScreen
-import com.rushar.newsapp.presentation.onboarding.OnBoardingViewModel
+import androidx.core.view.WindowCompat
+import com.rushar.newsapp.presentation.navgraph.NavGraph
 import com.rushar.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var useCases: AppEntryUseCases
+    val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
+        WindowCompat.setDecorFitsSystemWindows(window,false)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                viewModel.splashCondition.value
+            }
+        }
+
         setContent {
             NewsAppTheme {
                 Box(modifier = Modifier.background(color = MaterialTheme.colors.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(
-                        event = viewModel::onEvent
-                    )
+                    val startDestination = viewModel.startDestination.value
+                    NavGraph(startDestination = startDestination)
                 }
             }
         }
--- app/src/main/java/com/rushar/newsapp/presentation/onboarding/OnBoardingViewModel.kt
+++ app/src/main/java/com/rushar/newsapp/presentation/onboarding/OnBoardingViewModel.kt
@@ -3,9 +3,11 @@ package com.rushar.newsapp.presentation.onboarding
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.rushar.newsapp.domain.usecases.AppEntryUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class OnBoardingViewModel @Inject constructor(
     private val appEntryUseCases: AppEntryUseCases
 ) : ViewModel() {
